e5952b4872319e20a5af7650ea9046e891fedf3c
Luis Rogelio Lopez Ulloa <lr.lopezulloa@gmail.com>
2018-03-25 16:41:16 -0400

    Se agrego un formulario con campo title. Se escucha el evento submit para obtener la data del formulario.

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,1 +1,10 @@
-console.log('hola que ace')
+const $form = document.getElementById('form')
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+  // Prohibe que se haga lo que hace por defecto, en este cado recargar la pagina
+  event.preventDefault();
+  const data = new FormData($form)
+  const title = data.get('title')
+  console.log(title)
+}
