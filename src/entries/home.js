fe27728035e92b98470f0a3281c1e1d2b0cc1d68
Luis Rogelio Lopez Ulloa <lr.lopezulloa@gmail.com>
2018-03-25 19:32:54 -0400

    Se actualizo la forma en la que se consultan los datos normalizados

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
-import data from '../api.json';
+// import data from '../api.json';
 // console.log('Hola mundo!' )
-import normalizedData from '../schemas/index.js';
+import data from '../schemas/index.js';
 import {Provider} from 'react-redux';
 
 import {createStore} from 'redux';
 import reducer from '../reducers/data'
 
-console.log(normalizedData)
+
 
 const initialState = {
     data: {
-        ...data,  
+        // ...data,  
+        entities: data.entities,
+        categories: data.result.categories
     },
     search: [],
 }
